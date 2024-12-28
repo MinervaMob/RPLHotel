2d81ee7ed83fcb98ba148e33d7252c40864e3ebb
Ilham Pangestu <140824958+MinervaMob@users.noreply.github.com>
2024-12-28 16:17:01 +0700

    update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -67,7 +67,7 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
 
-    <title>RPL - Admin </title>
+    <title>RPL Hotel - Admin </title>
 </head>
 <body>
    <div class="databox">
